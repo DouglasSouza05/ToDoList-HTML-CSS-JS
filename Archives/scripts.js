0de65ab23d90814e4f6d48d247b9038b87f6d7dd
Douglas Souza <douglasbsouza01@gmail.com>
2023-07-05 18:00:11 -0300

    Implementação do add task button

--- Archives/scripts.js
+++ Archives/scripts.js
@@ -1,8 +1,11 @@
 // Código JavaScript
 
-// Criando as variáveis inputText e addTaskButton para receber as entradas
+// Criando as variáveis inputText e addTaskButton para receber as entradas. OBS: usar o "." e os nomes devem ser os mesmos criados no HTML
 const inputText = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
+
+// Criando a variável do .task-container para adicionar os elementos de input dentro
+const taskContainer = document.querySelector(".task-container");
 
 // const validateInputs = () => {
 //   if (inputText.value.trim() > 0) {
@@ -33,6 +36,30 @@ const handleAddTask = () => {
   if (!validInput) {
     return inputText.classList.add("error");
   }
+
+  // Criando o ".tasks-container". Usando document.createElemente para criar os elementos
+  // Nesse caso, criando uma "div"
+  const taskItemContainer = document.createElement("div");
+  // Adicionando a classe do elemento div
+  taskItemContainer.classList.add("task-item");
+
+  // Criando o parágrafo, conteúdo em si, que irá dentro da div. Não necessário criar uma classe pois não será estilizado
+  const taskContent = document.createElement("p");
+  // Definindo o innerText do parágrafo como sendo o "value" da entrada inputText
+  taskContent.innerText = inputText.value;
+
+  // Criando o ícone do delete, usando Font Awesome
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("fa-solid");
+  deleteItem.classList.add("fa-trash");
+
+  // Colocando o parágrafo e o ícone criados dentro da div .task-item
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  taskContainer.appendChild(taskItemContainer);
+
+  inputText.value = "";
 };
 
 // Arrow function usada para verificar a mudança na entrada de dados do elemento inputText
